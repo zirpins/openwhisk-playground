ea436f2345d351bae1aef982dbc6e30b219dc1b0
Christian Zirpins <christian.zirpins@hs-karlsruhe.de>
2017-05-01 20:32:05 +0200

    update

--- travis-test.py
+++ travis-test.py
@@ -22,20 +22,27 @@ print("FIBONACCI_API_URL is: " + FIBONACCI_API_URL)
 
 
 def main():
-    checkFibonacci(0, -1)
+    checkBadIndex()
     checkFibonacci(25, 75025)
 
+def checkBadIndex():
+    print("Verify that a bad index (below 1) will fail (GET)")
+    response = requests.get(FIBONACCI_API_URL+"?index=-1")
+    result = json.loads(response.text)
+    print("Response: (" + str(response.status_code) + ") " + response.text)
+    if response.status_code != 502 or "error" not in result or result["error"] != "index too small":
+        print("Failed to verify bad index.")
+        exit(1)
+    print("success")
 
 def checkFibonacci(index, number):
     print("Verify that 25th fibonacci number is 75025 (GET)")
     response = requests.get(FIBONACCI_API_URL+"?index="+str(index))
     result = json.loads(response.text)
-    print("Response: " + response.text)
-
+    print("Response: (" + str(response.status_code) + ") " + response.text)
     if response.status_code != 200 or 'index' not in result or 'provider' not in result or result["index"] != index or result["number"] != number:
-        print("Failed to verify fibonacci computation.)
+        print("Failed to verify fibonacci computation.")
         exit(1)
-
     print("success")
 
 main()
